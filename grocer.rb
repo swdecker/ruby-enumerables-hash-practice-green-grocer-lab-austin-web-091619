b31861923bbe26042336faa23dbac712583ff9c4
Steven Decker <swdecker@gmail.com>
2019-09-04 21:31:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,8 @@ end
 
 def apply_clearance(cart)
   # code here
-  cart.map do |item_hash|
+  ccart = consolidate_cart(cart)
+  ccart.map do |item_hash|
     item = item_hash.keys[0]
     if cart[item][:clearance]
       cart[item][:price] *= 0.8
